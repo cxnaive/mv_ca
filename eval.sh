33b6b402bfdd3c7f22ad6d203aa5aa18db0c947a
cxnaive <1012033254@qq.com>
2022-11-02 05:32:12 +0000

    eval results

---
+++ eval.sh
@@ -0,0 +1,2 @@
+python tools/test.py configs/recognition/csn/ircsn_dark_dataset.py \
+    work_dirs/ircsn_dark_dataset/latest.pth --eval top_k_accuracy mean_class_accuracy \
--- mmaction_test.py
+++ mmaction_test.py
@@ -9,12 +9,12 @@ with open(class_labels,'r') as f:
 config_file = 'mmaction2/configs/recognition/csn/ircsn_dark_dataset.py'
 device = 'cuda:0' # or 'cpu'
 device = torch.device(device)
-chk_point_file = '/root/mv_ca/mmaction2/work_dirs/ircsn_dark_dataset/epoch_58.pth'
+chk_point_file = '/root/mv_ca/mmaction2/work_dirs/ircsn_dark_dataset/latest.pth'
 
 model = init_recognizer(config_file,chk_point_file, device=device)
 print('init done.')
 # inference the demo video
-results = inference_recognizer(model, 'Drink_3_2.mp4')
+results = inference_recognizer(model, 'datasets/validate/0.mp4')
 for res in results:
     print(res)
     print(class_list[res[0]])
